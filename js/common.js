4e94a1a98137e913f2358d8cf74144ba3d77a62a
yewul01 <jjoham234@daum.net>
2020-09-11 10:47:45 +0900

    헤더부분 수정

--- js/common.js
+++ js/common.js
@@ -16,15 +16,7 @@
         $('#joBox').load(url)
     })
 
-    // 헤더 자연스럽게 열기
-    $('.depth1 > li').hover(
-        function(){
-            $(this).find('.depth2').stop().slideDown(700)
-        },
-        function(){
-            $(this).find('.depth2').stop().slideUp(700)
-        }
-    )
+    
     
 
 
