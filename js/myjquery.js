e4e578837509c062365bede3d5884653b989ff33
yewul01 <jjoham234@daum.net>
2020-09-11 18:25:44 +0900

    자바

--- js/myjquery.js
+++ js/myjquery.js
@@ -2,33 +2,33 @@
 
     $(window).scroll(function (){
         var sct = $(this).scrollTop()
-        var company = $('.company').offset().top
-        var banner = $('.Quality_banner').offset().top
-        var news = $('.news').offset().top
-        var product = $('.product').offset().top
+        var company = $('#section .company').offset().top
+        var banner = $('#section .Quality_banner').offset().top
+        var news = $('#section .news').offset().top
+        var product = $('#section .product').offset().top
 
         if ( sct >= company ) {
-            $('.section .company').addClass('on')
+            $('#section .company').addClass('on')
         } else {
-            $('.section .company').removeClass('on')
+            $('#section .company').removeClass('on')
         }
 
         if (sct >= banner ) {
-            $('.section .Quality_banner').addClass('on')
+            $('#section .Quality_banner').addClass('on')
         } else {
-            $('.section .Quality_banner').removeClass('on')
+            $('#section .Quality_banner').removeClass('on')
         }
 
         if ( sct >= news ) {
-            $('section .news').addClass('on')
+            $('#section .news').addClass('on')
         } else {
-            $('section .news').removeClass('on')
+            $('#section .news').removeClass('on')
         }
 
         if ( sct >= product ) {
-            $('section .product').addClass('on')
+            $('#section .product').addClass('on')
         } else {
-            $('section .product').removeClass('on')
+            $('#section .product').removeClass('on')
         }
     })
 
