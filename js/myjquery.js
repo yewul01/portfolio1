351f6fcd960b00beff60ea5c6aae57c4516ab731
yewul01 <jjoham234@daum.net>
2020-09-11 18:27:54 +0900

    company 자바

--- js/myjquery.js
+++ js/myjquery.js
@@ -1,6 +1,6 @@
 (function($){
 
-    $(window).scroll(function (){
+    $(window).scroll(function(){
         var sct = $(this).scrollTop()
         var company = $('#section .company').offset().top
         var banner = $('#section .Quality_banner').offset().top
@@ -9,27 +9,19 @@
 
         if ( sct >= company ) {
             $('#section .company').addClass('on')
-        } else {
-            $('#section .company').removeClass('on')
-        }
+        } 
 
         if (sct >= banner ) {
             $('#section .Quality_banner').addClass('on')
-        } else {
-            $('#section .Quality_banner').removeClass('on')
-        }
+        } 
 
         if ( sct >= news ) {
             $('#section .news').addClass('on')
-        } else {
-            $('#section .news').removeClass('on')
-        }
+        } 
 
         if ( sct >= product ) {
             $('#section .product').addClass('on')
-        } else {
-            $('#section .product').removeClass('on')
-        }
+        } 
     })
 
 
