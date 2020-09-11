7bed01881917696021396a0e7920683c02645912
yewul01 <jjoham234@daum.net>
2020-09-11 17:56:32 +0900

    수정중

--- js/myjquery.js
+++ js/myjquery.js
@@ -1,3 +1,19 @@
 (function($){
-    
+
+
+    $('.section').on('mousewheel', function(e, wh) {
+        if (wh > 0) {
+            var prev = $(this).prev().offset().top
+            $('html,body').stop().animate({
+                scrollTop:prev
+            }, 800)
+        } else if (wh < 0) {
+            var next = $(this).next().offset().top
+            $('html,body').stop().animate({
+                scrollTop:next
+            }, 800)
+        }
+    })
+
+
 })(jQuery)
