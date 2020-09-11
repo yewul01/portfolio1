3ede86a817c634263c8cdb1fbdac75ad0faf5b2e
yewul01 <jjoham234@daum.net>
2020-09-11 10:46:49 +0900

    헤더 수정

--- js/common.js
+++ js/common.js
@@ -19,10 +19,10 @@
     // 헤더 자연스럽게 열기
     $('.depth1 > li').hover(
         function(){
-            $(this).find('.depth2').stop().slideDown(500)
+            $(this).find('.depth2').stop().slideDown(700)
         },
         function(){
-            $(this).find('.depth2').stop().slideUp(500)
+            $(this).find('.depth2').stop().slideUp(700)
         }
     )
     
