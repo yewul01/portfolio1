ce32f8aabc353fb59d63c0f883fdba49a272f23c
yewul01 <jjoham234@daum.net>
2020-09-11 14:18:06 +0900

    메인 페이지 수정중

--- js/common.js
+++ js/common.js
@@ -9,7 +9,14 @@
         $('#joBox').load(url)
     })
 
-    $('.nav > ul > li:nth-child(4) > a').on('click', function(e) {
+    $('.nav ul li:nth-child(4) a').on('click', function(e) {
+        e.preventDefault()
+        var url = $(this).attr('href')
+        $('#joContainer').remove()
+        $('#joBox').load(url)
+    })
+
+    $('.customer ul li:nth-child(1) a').on('click', function(e) {
         e.preventDefault()
         var url = $(this).attr('href')
         $('#joContainer').remove()
