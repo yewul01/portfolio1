70d534fd8ecb38cc7b1a46e7e8765ef493ce2c8e
yewul01 <jjoham234@daum.net>
2020-09-13 18:00:17 +0900

    메이 페이지 자바 액션

--- js/myjquery.js
+++ js/myjquery.js
@@ -3,24 +3,28 @@
     $(window).scroll(function(){
         var sct = $(this).scrollTop()
         var company = $('.company').offset().top
-        var banner = $('.Quality_banner').offset().top
-        var news = $('.news').offset().top
-        var product = $('.product').offset().top
+        var banner = $('.company ul li').offset().top
+        var news = $('.Quality_banner').offset().top
+        var newsBtn = $('.news').offset().top
+        var product = $('.news').offset().top
 
         if ( sct >= company ) {
             $('.company').addClass('on')
-        } 
+        }
 
         if (sct >= banner ) {
-            $('.Quality_banner').addClass('on')
+            $('.Quality_text').addClass('on')
         } 
 
         if ( sct >= news ) {
-            $('.news').addClass('on')
+            $('.news_intr').addClass('on')
+        }
+        if ( sct >= newsBtn ) {
+            $('.news .button').addClass('on')
         } 
 
         if ( sct >= product ) {
-            $('.product').addClass('on')
+            $('.prod_intr').addClass('on')
         } 
     })
 
