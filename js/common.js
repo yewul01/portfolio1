c11d493cef294291bb5b1bacbb381710d52c9cb3
yewul01 <jjoham234@daum.net>
2020-09-14 11:25:06 +0900

    로드 페이지

--- js/common.js
+++ js/common.js
@@ -1,4 +1,19 @@
 (function($){
+    var counter = 0;
+    var c = 0;
+    var i = setInterval(function() {
+        $('.location_box h1').html(c + '%')
+        counter++;
+        c++;
+        if (counter == 101) {
+            clearInterval(i);
+        }
+    }, 5)
+
+    $(window).load(function() {
+        $('.introAni').delay(500).fadeOut(500)
+    })
+    
 
     $('#joBox').load('main.html')
 
