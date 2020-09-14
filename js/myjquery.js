a97862d2b7303ee9b7e612a79f2f85db79bb8d78
yewul01 <jjoham234@daum.net>
2020-09-14 17:00:39 +0900

    메인 페이지 자바 액션

--- js/myjquery.js
+++ js/myjquery.js
@@ -1,16 +1,32 @@
 (function($){
-
+    
+    // 메인 페이지 애니메이션
     $(window).scroll(function() {
-        var sct = $(this).scrollTop()
+        var sct = $(this).scrollTop() 
         var comNear = $('.company').offset().top - $(this).height()/2
         if (sct >= comNear) {
             $('.company .comul .comli').addClass('fadeInUp')
             for (var i=0; i<$('.comul .comli').length; i++) {
                 $('.comul .comli').eq(i).css({
-                    animationDelay:i*0.3 + 's'
+                    animationDelay:i*0.1 + 's'
                 }).addClass('fadeInUp')
             }
         } else { $('.company .comul .comli').removeClass('fadeInUp') }
+        
+
+        if (sct >= comNear) {
+            $('.company h2').addClass('fadeInUp')
+        } else {
+            $('.company h2').removeClass('fadeInUp')
+        }
+
+        // Quality 배너 애니메이션
+        var QualNear = $('.Quality_banner').offset().top - $(this).height()/2
+        if (sct >= QualNear) {
+            $('.Quality_text').addClass('on')
+        } else {
+            $('.Quality_text').removeClass('on')
+        }
     })
 
 
