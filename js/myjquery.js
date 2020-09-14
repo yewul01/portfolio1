4d9ae162ae3b56bde914d7af95d2a93c9fa8ee3d
yewul01 <jjoham234@daum.net>
2020-09-14 14:46:56 +0900

    company 자바 액션

--- js/myjquery.js
+++ js/myjquery.js
@@ -2,30 +2,12 @@
 
     $(window).scroll(function(){
         var sct = $(this).scrollTop()
-        var company = $('.company').offset().top
-        var banner = $('.company ul li').offset().top
-        var news = $('.Quality_banner').offset().top
-        var newsBtn = $('.news').offset().top
-        var product = $('.news').offset().top
-
-        if ( sct >= company ) {
-            $('.company').addClass('on')
+        var comNear = $('company').offset().top - $(this).height()/2
+        if (sct >= comNear) {
+            $('.company .comul .comli').addClass('fadeInUp')
+        } else {
+            $('.company .comul .comli').removeClass('fadeInUp')
         }
-
-        if (sct >= banner ) {
-            $('.Quality_text').addClass('on')
-        } 
-
-        if ( sct >= news ) {
-            $('.news_intr').addClass('on')
-        }
-        if ( sct >= newsBtn ) {
-            $('.news .button').addClass('on')
-        } 
-
-        if ( sct >= product ) {
-            $('.prod_intr').addClass('on')
-        } 
     })
 
 
