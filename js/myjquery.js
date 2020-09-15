4585ed7ef3d601c15cf8ff2737d3d6ecd87280e7
yewul01 <jjoham234@daum.net>
2020-09-15 10:32:56 +0900

    헤더 컨텐츠 겹침현상 수정

--- js/myjquery.js
+++ js/myjquery.js
@@ -2,7 +2,12 @@
     
     // 메인 페이지 애니메이션
     $(window).scroll(function() {
-        var sct = $(this).scrollTop() 
+
+
+        var sct = $(this).scrollTop()
+
+
+        // company li 애니메이션 
         var comNear = $('.company').offset().top - $(this).height()/2
         if (sct >= comNear) {
             $('.company .comul .comli').addClass('fadeInUp')
@@ -14,11 +19,26 @@
         } else { $('.company .comul .comli').removeClass('fadeInUp') }
         
 
+
+        // 헤더, 컨텐츠 겹침현상 수정
+        var company = $('.company').offset().top
+        if (sct >= company) {
+            $('.h1NavTop').addClass('on')
+        } else {
+            $('.h1NavTop').removeClass('on')
+        }
+        
+
+
+        // company 텍스트 애니메이션
+
         if (sct >= comNear) {
             $('.company h2').addClass('fadeInUp')
         } else {
             $('.company h2').removeClass('fadeInUp')
         }
+
+
 
         // Quality 배너 애니메이션
         var QualNear = $('.Quality_banner').offset().top - $(this).height()/2
@@ -26,6 +46,36 @@
             $('.Quality_text').addClass('on')
         } else {
             $('.Quality_text').removeClass('on')
+        }
+        
+
+
+        // news 텍스트 애니메이션
+        var news = $('.news').offset().top - $(this).height()/2
+        if (sct >= news) {
+            $('.news_intr').addClass('on')
+        } else {
+            $('.news_intr').removeClass('on')
+        }
+
+
+
+        // news 더보기 버튼 애니메이션
+        var news_intr = $('.news_intr').offset().top - $(this).height()/2
+        if (sct >= news_intr) {
+            $('.newsButton').addClass('on')
+        } else {
+            $('.newsButton').removeClass('on')
+        }
+
+
+
+        // Product 텍스트 애니메이션
+        var product = $('.news_intr').offset().top - $(this).height()/2
+        if (sct >= product) {
+            $('.prod_intr').addClass('on')
+        } else {
+            $('.prod_intr').removeClass('on')
         }
     })
 
