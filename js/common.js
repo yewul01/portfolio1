4e4c69a6cbe8ff85a6cc68107fab280b73dee073
yewul01 <68521830+yewul01@users.noreply.github.com>
2020-09-15 11:20:50 +0900

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,4 +1,5 @@
 (function($){
+    
     var counter = 0;
     var c = 0;
     var i = setInterval(function() {
@@ -6,14 +7,13 @@
         counter++;
         c++;
         if (counter == 101) {
-            clearInterval(i);
+            clearInterval(i)
         }
     }, 5)
 
     $(window).load(function() {
         $('.introAni').delay(500).fadeOut(500)
     })
-    
 
     $('#joBox').load('main.html')
 
@@ -24,45 +24,14 @@
         $('#joBox').load(url)
     })
 
-    $('.nav ul li:nth-child(4) a').on('click', function(e) {
+    $('.customer > a').on('click', function(e) {
         e.preventDefault()
         var url = $(this).attr('href')
         $('#joContainer').remove()
         $('#joBox').load(url)
     })
-
-    $('.customer ul li:nth-child(1) a').on('click', function(e) {
-        e.preventDefault()
-        var url = $(this).attr('href')
-        $('#joContainer').remove()
-        $('#joBox').load(url)
-    })
-
     
-    $(window).scroll(function() {
 
-        var sct = $(this).scrollTop()
-
-        // 메인 페이지 헤더, 컨텐츠 겹침현상 수정
-        var company = $('.company').offset().top
-        if (sct >= company) {
-            $('.h1NavTop').addClass('on')
-        } else {
-            $('.h1NavTop').removeClass('on')
-        }
-        
-
-        // customer 페이지 헤더, 컨텐츠 겹침현상 수정
-        var cust_slide = $('.cust_slide').offset().top
-        if (sct >= cust_slide) {
-            $('.h1NavTop').addClass('on')
-        } else {
-            $('.h1NavTop').removeClass('on')
-        }
-
-        
-    })
-    
 
 
 
