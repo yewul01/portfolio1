dfdb9617f557ae22871f67472aa26c923b83f4b9
yewul01 <jjoham234@daum.net>
2020-09-21 19:58:19 +0900

    toTop버튼

--- js/header.js
+++ js/header.js
@@ -16,26 +16,76 @@
 
     $(window).scroll(function() { 
         var sct = $(this).scrollTop() 
+        var company = $('.company').offset().top
+    
         // 메인 페이지 헤더, 컨텐츠 겹침현상 수정 
-        var company = $('.company').offset().top 
         if (sct >= company) { 
             $('.h1NavTop').addClass('on') 
         } else { 
             $('.h1NavTop').removeClass('on') 
-        } 
-         
+        }
+    })
 
-        // // customer 페이지 헤더, 컨텐츠 겹침현상 수정 
-        // var cust_slide = $('.cust_slide').offset().top 
-        // if (sct >= cust_slide) { 
-        //     $('.h1NavTop').addClass('on') 
-        // } else { 
-        //     $('.h1NavTop').removeClass('on') 
-        // } 
- 
+    $(window).scroll(function() { 
+        var sct = $(this).scrollTop() 
+        var Map = $('.map_slide').offset().top
+        
+        // 오시는길
+        if (sct >= Map) { 
+            $('.h1NavTop').addClass('on') 
+        } else { 
+            $('.h1NavTop').removeClass('on') 
+        }
+        
+    })
 
-         
-    }) 
+    $(window).scroll(function() { 
+        var sct = $(this).scrollTop() 
+        var customer = $('.cust_slide').offset().top
+       
+        // 공지사항
+        if (sct >= customer) { 
+            $('.h1NavTop').addClass('on') 
+        } else { 
+            $('.h1NavTop').removeClass('on') 
+        }
+    })
+
+    $(window).scroll(function() { 
+        var sct = $(this).scrollTop() 
+        var QA = $('.cust2_slide')
+       
+        // Q&A
+        if (sct >= QA) { 
+            $('.h1NavTop').addClass('on') 
+        } else { 
+            $('.h1NavTop').removeClass('on') 
+        }
+    })
+
+    $(window).scroll(function() { 
+        var sct = $(this).scrollTop() 
+        var login = $('.log_slide')
+
+        // 로그인
+        if (sct >= login) { 
+            $('.h1NavTop').addClass('on') 
+        } else { 
+            $('.h1NavTop').removeClass('on') 
+        }   
+    })
+
+    $(window).scroll(function() { 
+        var sct = $(this).scrollTop()   
+        var join2 = $('.join2_slide')
+        
+        // 회원가입
+        if (sct >= join2) { 
+            $('.h1NavTop').addClass('on') 
+        } else { 
+            $('.h1NavTop').removeClass('on') 
+        }   
+    })
 
 
 
--- js/myjquery.js
+++ js/myjquery.js
@@ -5,16 +5,16 @@
     $(window).scroll(function() {
 
         var sct = $(this).scrollTop()
+        console.log(sct)
 
         
         // company 텍스트 애니메이션
         var comNear = $('.company').offset().top - $(this).height()/2
         if (sct >= comNear) {
             $('.company h2').addClass('on')
-        } else {
+        } else if (sct===0) {
             $('.company h2').removeClass('on')
         }
-
 
 
         // company li 애니메이션 
@@ -26,49 +26,65 @@
                     animationDelay:i*0.1 + 's'
                 }).addClass('animate__fadeInUp')
             }
-        } else { $('.company .comul .comli').removeClass('animate__fadeInUp') }
+        } else if (sct===0) { 
+            $('.company .comul .comli').removeClass('animate__fadeInUp')
+        }
         
-
-
-
-
 
         // Quality 배너 애니메이션
         var QualNear = $('.Quality_banner').offset().top - $(this).height()/2
         if (sct >= QualNear) {
             $('.Quality_text').addClass('on')
-        } else {
+        } else if (sct===0) {
             $('.Quality_text').removeClass('on')
         }
         
 
-
         // news 텍스트 애니메이션
         if (sct >= QualNear) {
             $('.news_intr').addClass('on')
-        } else {
+        } else if (sct===0) {
             $('.news_intr').removeClass('on')
         }
-
 
 
         // news 더보기 버튼 애니메이션
         var news_intr = $('.news_intr').offset().top - $(this).height()/2
         if (sct >= news_intr) {
             $('.newsButton').addClass('on')
-        } else {
+        } else if (sct===0) {
             $('.newsButton').removeClass('on')
         }
-
 
 
         // Product 텍스트 애니메이션
         if (sct >= news_intr) {
             $('.prod_intr').addClass('on')
-        } else {
+        } else if (sct===0) {
             $('.prod_intr').removeClass('on')
         }
+
+        if (sct >= 10) {
+            $('.toTop').addClass('on').stop().animate({
+                opacity: '1'
+            }, 300)
+        } else {
+            $('.toTop').removeClass('on').stop().animate({
+                opacity: '0'
+            }, 300)
+        }
+
+        // toTop 버튼 이벤트
+    $('.toTop').on('click',function () {
+        $('html, body').stop().animate({
+            scrollTop: '0'
+        }, 800, 'linear')
     })
+
+})
+    
+
+    
 
 
     
