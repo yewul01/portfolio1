a8abf8563f1a2e2a6ac7d2849bea7a4ccc1696b4
yewul01 <jjoham234@daum.net>
2020-10-12 17:40:17 +0900

    메인 페이지 반응형 수정

--- js/common.js
+++ js/common.js
@@ -266,99 +266,68 @@
     })
 
 
-
     // 모바일화면에서 1단계메뉴 클릭했을때 2단계메뉴 보이게 하기
-  $(".depth1 > li > a").on('click', function(){
-    if ( $('html').hasClass('mobile') ) {
-      $(this).parent().toggleClass('on')
-      $(this).parent().find('.depth2').stop().slideToggle(300)
-      $(this).parent().siblings().each(function(){
-          if (  $(this).find('.depth2').css('display') === 'block' ) {
-            $(this).find('.depth2').slideUp(300)
-            $(this).removeClass('on')
-          }
-      })
-    }
-})
-
-// 모바일화면에서 1단계메뉴 클릭했을때 2단계메뉴 보이게 하고,
-// 2단계 메뉴가 없으면 1단계메뉴 페이지 로드시키기
-//   $(".depth1 > li > a").on("click", function (e) {
-//     e.preventDefault();
-//     if ($("html").hasClass("mobile")) {
-//       if ($(this).next().is(".depth2")) {
-//         $(this).parent().toggleClass("on");
-//         $(this).parent().find(".depth2").stop().slideToggle(300);
-//         $(this)
-//         .parent()
-//           .siblings()
-//           .each(function () {
-//             if ($(this).find(".depth2").css("display") === "block") {
-//               $(this).find(".depth2").slideUp(300);
-//               $(this).removeClass("on");
-//             }
-//           });
-//       } else if (!$(this).next().is(".depth2")) {
-//         var url = $(this).attr("href");
-//         $("#joContainer").remove();
-//         $("#joBox").load(url);
-//         $(".open_nav").show();
-//         $(".h1NavTop .nav, .close_nav").hide();
-//         $(".depth1 > li").removeClass("on");
-//       }
-//     } else if ($("html").hasClass("pc")) {
-//       var url = $(this).attr("href");
-//       $("#joContainer").remove();
-//       $("#joBox").load(url);
-//     }
-//   });
-
-// pc화면에서 1단계메뉴에 호버했을때 2단계메뉴 보이게 하기
-$('.depth1 > li').hover(
-  function(){
-    if ( $('html').hasClass('pc') ) {
-      $(this).find('.depth2, .decoBox').stop().slideDown(300)
-    }
-  },
-  function(){
-    if ( $('html').hasClass('pc') ) {
-      $(this).find('.depth2, .decoBox').stop().slideUp(300)
-    }
-  }
-)
-
-// 2단계 메뉴 클릭하면 모든 화면에서 페이지 로드시킨 후, 모바일화면에서는 햄버거 버튼만 보이게 하기
-$(".nav .depth2 > li > a").on("click", function (e) {
-    e.preventDefault();
-    var url = $(this).attr("href");
-    $("#joContainer").remove();
-    $("#joBox").load(url);
-    if ($("html").hasClass("mobile")) {
-      $(".open_nav").show();
-      $(".h1NavTop .nav, .depth2, .close_nav").hide();
-      $(".depth1 > li").removeClass("on");
-    }
-  });
+    $(".depth1 > li > a").on('click', function(){
+        if ( $('html').hasClass('mobile') ) {
+          $(this).parent().toggleClass('on')
+          $(this).parent().find('.depth2').stop().slideToggle(300)
+          $(this).parent().siblings().each(function(){
+              if (  $(this).find('.depth2').css('display') === 'block' ) {
+                $(this).find('.depth2').slideUp(300)
+                $(this).removeClass('on')
+              }
+          })
+        }
+        })
 
 
-// 햄버거버튼 클릭시 네비박스 나타나기
- $('.h1NavTop .open_nav').on('click',function() {
-   $(this).next().stop().slideDown(300)
-   $(this).hide()
-   $(this).nextAll('.close_nav').css({
-       display: 'block'
-   })
-})
 
-// 닫기버튼 클릭시 네비박스 사라지기
-$('.h1NavTop .close_nav').on('click',function() {
-    $(this).prev().prev().stop().slideUp(300)
-    $(this).hide()
-    $(this).prevAll('.open_nav').css({
-        display: 'block'
+    // pc화면에서 1단계메뉴에 호버했을때 2단계메뉴 보이게 하기
+    $('.depth1 > li').hover(
+      function(){
+        if ( $('html').hasClass('pc') ) {
+          $(this).find('.depth2, .decoBox').stop().slideDown(300)
+        }
+      },
+      function(){
+        if ( $('html').hasClass('pc') ) {
+          $(this).find('.depth2, .decoBox').stop().slideUp(300)
+        }
+      }
+    )
+
+    // 2단계 메뉴 클릭하면 모든 화면에서 페이지 로드시킨 후, 모바일화면에서는 햄버거 버튼만 보이게 하기
+    $(".nav .depth2 > li > a").on("click", function (e) {
+        e.preventDefault();
+        var url = $(this).attr("href");
+        $("#joContainer").remove();
+        $("#joBox").load(url);
+        if ($("html").hasClass("mobile")) {
+          $(".open_nav").show();
+          $(".h1NavTop .nav, .depth2, .close_nav").hide();
+          $(".depth1 > li").removeClass("on");
+        }
+      });
+
+
+    // 햄버거버튼 클릭시 네비박스 나타나기
+     $('.h1NavTop .open_nav').on('click',function() {
+       $(this).next().stop().slideDown(300)
+       $(this).hide()
+       $(this).nextAll('.close_nav').css({
+           display: 'block'
+       })
     })
-    $('.depth2').hide()
-})
+
+    // 닫기버튼 클릭시 네비박스 사라지기
+    $('.h1NavTop .close_nav').on('click',function() {
+        $(this).prev().prev().stop().slideUp(300)
+        $(this).hide()
+        $(this).prevAll('.open_nav').css({
+            display: 'block'
+        })
+        $('.depth2').hide()
+    })
  
 
 
