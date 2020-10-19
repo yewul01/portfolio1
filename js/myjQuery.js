1b58094bd21cc47e6db19662372e99c2e1fdbfdf
yewul01 <jjoham234@daum.net>
2020-10-19 09:59:09 +0900

    모바일 화면에서 1단계메뉴 클릭했을때

--- js/myjQuery.js
+++ js/myjQuery.js
@@ -264,22 +264,25 @@
         $(this).parent().addClass('active')
         $(this).parent().siblings().removeClass('active')
     })
-
-
+    
     // 모바일화면에서 1단계메뉴 클릭했을때 2단계메뉴 보이게 하기
-    $(".depth1 > li > a").on('click', function(){
+    $(".depth1 > li > a").on('click', function(e){
+        e.preventDefault()
         if ( $('html').hasClass('mobile') ) {
-            if ( $(this).next().is('.depth2') )
-          $(this).parent().toggleClass('on')
+          $(this).toggleClass('on')
           $(this).parent().find('.depth2').stop().slideToggle(300)
           $(this).parent().siblings().each(function(){
-              if (  $(this).css('display') === 'block' ) {
+            if ( $(this).css('display') === 'block' ) {
                 $(this).find('.depth2').slideUp(300)
                 $(this).removeClass('on')
-              }
+            } else if ($('html').hasClass('pc')){
+                var url = $(this).attr('href')
+                $('#joContainer').remove()
+                $('#joBox').load(url)
+            }
           })
         }
-        })
+    })
 
 
 
